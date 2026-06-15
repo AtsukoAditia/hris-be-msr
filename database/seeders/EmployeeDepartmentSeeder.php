b692e05148f120d04db9eee9bdfb3147978a9422
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-15 13:36:35 +0700

    feat: seed transition departments before Employee backfill

--- database/seeders/EmployeeDepartmentSeeder.php
+++ database/seeders/EmployeeDepartmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use App\Models\Employee;
 use App\Services\EmployeeDepartmentResolver;
 use Illuminate\Database\Seeder;
@@ -10,6 +11,8 @@ class EmployeeDepartmentSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->ensureTransitionDepartments();
+
         $resolver = app(EmployeeDepartmentResolver::class);
 
         Employee::query()
@@ -25,5 +28,36 @@ class EmployeeDepartmentSeeder extends Seeder
                     'department_id' => $department->id,
                 ]);
             });
+    }
+
+    private function ensureTransitionDepartments(): void
+    {
+        $departments = [
+            [
+                'code' => 'MGT',
+                'name' => 'Management',
+                'description' => 'Mengelola arah strategis, tata kelola, dan pengambilan keputusan perusahaan.',
+            ],
+            [
+                'code' => 'MKT',
+                'name' => 'Marketing',
+                'description' => 'Mengelola pemasaran, komunikasi, promosi, dan pertumbuhan pasar.',
+            ],
+        ];
+
+        foreach ($departments as $data) {
+            $department = Department::withTrashed()->firstOrNew([
+                'code' => $data['code'],
+            ]);
+
+            if ($department->exists && $department->trashed()) {
+                $department->restore();
+            }
+
+            $department->fill([
+                ...$data,
+                'is_active' => true,
+            ])->save();
+        }
     }
 }
