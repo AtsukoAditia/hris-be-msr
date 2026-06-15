75fe726e9b0aafaca91f3d1844c812434080e71b
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-15 14:36:21 +0700

    feat: register Position seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             EmployeeSeeder::class,
             EmployeeDepartmentSeeder::class,
+            PositionSeeder::class,
+            EmployeePositionSeeder::class,
         ]);
     }
 }
