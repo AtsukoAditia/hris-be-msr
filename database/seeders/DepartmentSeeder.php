5f335679b04be2037b6cdd5d13e51c91edefdf27
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-15 13:16:18 +0700

    feat: add idempotent Department seeder

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Department;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $departments = [
+            [
+                'code' => 'IT',
+                'name' => 'Information Technology',
+                'description' => 'Mengelola sistem, aplikasi, infrastruktur, dan dukungan teknologi perusahaan.',
+            ],
+            [
+                'code' => 'HR',
+                'name' => 'Human Resources',
+                'description' => 'Mengelola administrasi, pengembangan, dan hubungan karyawan.',
+            ],
+            [
+                'code' => 'FIN',
+                'name' => 'Finance',
+                'description' => 'Mengelola keuangan, anggaran, pembayaran, dan pelaporan perusahaan.',
+            ],
+            [
+                'code' => 'OPS',
+                'name' => 'Operations',
+                'description' => 'Mengelola kegiatan operasional dan proses bisnis harian.',
+            ],
+        ];
+
+        foreach ($departments as $data) {
+            $department = Department::withTrashed()->firstOrNew([
+                'code' => $data['code'],
+            ]);
+
+            if ($department->exists && $department->trashed()) {
+                $department->restore();
+            }
+
+            $department->fill([
+                ...$data,
+                'is_active' => true,
+            ])->save();
+        }
+    }
+}
