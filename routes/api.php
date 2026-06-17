a78b28f700eeb62c4d009c02c39232bfa97d53da
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-17 14:05:18 +0700

    fix: prioritize custom shift schedule routes

--- routes/api.php
+++ routes/api.php
@@ -66,10 +66,12 @@ Route::prefix('v1')->group(function () {
             Route::put('/attendance/settings', [AttendanceSettingController::class, 'update']);
             Route::post('/attendance/qr/generate', [AttendanceSettingController::class, 'generateQr']);
             Route::apiResource('/shifts', ShiftController::class);
-            Route::apiResource('/shift-schedules', ShiftScheduleController::class);
+
             Route::get('/shift-schedules/employee/{employeeId}', [ShiftScheduleController::class, 'getByEmployee']);
             Route::get('/shift-schedules/date/{date}', [ShiftScheduleController::class, 'getByDate']);
             Route::post('/shift-schedules/bulk', [ShiftScheduleController::class, 'bulkStore']);
+            Route::apiResource('/shift-schedules', ShiftScheduleController::class);
+
             Route::get('/employees/manager-options', [EmployeeController::class, 'managerOptions']);
             Route::apiResource('/employees', EmployeeController::class)->except(['destroy']);
             Route::post('/employees/{employee}/face-enrollment', [EmployeeController::class, 'enrollFace']);
@@ -77,6 +79,11 @@ Route::prefix('v1')->group(function () {
             Route::get('/employees/{employee}/profile', [EmployeeController::class, 'profile']);
 
             Route::post('/departments', [DepartmentController::class, 'store']);
+            Route::put('/departments/{department}', [DepartmentController::class, 'update']);
+            Route::patch('/departments/{department}', [DepartmentController::class, 'update']);
+            Route::delete('/departments/{department}', [DepartmentController::class, 'destroy']);
+
+            Route::post('/positions', [PositionController::class, 'store']);
             Route::put('/departments/{department}', [DepartmentController::class, 'update']);
             Route::patch('/departments/{department}', [DepartmentController::class, 'update']);
             Route::delete('/departments/{department}', [DepartmentController::class, 'destroy']);
