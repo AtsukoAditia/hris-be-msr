b58eeff053361b5f18ce649c28abdde0429362b4
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-06-20 15:39:29 +0700

    feat: add payroll period resource

---
+++ app/Http/Resources/PayrollPeriodResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PayrollPeriodResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'start_date' => $this->start_date?->toDateString(),
+            'end_date' => $this->end_date?->toDateString(),
+            'cutoff_start_date' => $this->cutoff_start_date?->toDateString(),
+            'cutoff_end_date' => $this->cutoff_end_date?->toDateString(),
+            'status' => $this->status,
+            'payrolls_count' => $this->whenCounted('payrolls'),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
