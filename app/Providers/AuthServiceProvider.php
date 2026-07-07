d8e43a98811a67d706fbd00904916b29beda1888
Aditia Nugraha <atsuko.aditia@gmail.com>
2026-07-07 22:05:09 +0700

    style: import auth service provider policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,16 +2,26 @@
 
 namespace App\Providers;
 
+use App\Models\OvertimePolicy;
+use App\Models\OvertimeRequest;
+use App\Models\Payroll;
+use App\Models\PayrollPeriod;
+use App\Models\ShiftSchedule;
+use App\Policies\OvertimePolicyPolicy;
+use App\Policies\OvertimeRequestPolicy;
+use App\Policies\PayrollPeriodPolicy;
+use App\Policies\PayrollPolicy;
+use App\Policies\ShiftSchedulePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [
-        \App\Models\OvertimePolicy::class => \App\Policies\OvertimePolicyPolicy::class,
-        \App\Models\OvertimeRequest::class => \App\Policies\OvertimeRequestPolicy::class,
-        \App\Models\PayrollPeriod::class => \App\Policies\PayrollPeriodPolicy::class,
-        \App\Models\Payroll::class => \App\Policies\PayrollPolicy::class,
-        \App\Models\ShiftSchedule::class => \App\Policies\ShiftSchedulePolicy::class,
+        OvertimePolicy::class => OvertimePolicyPolicy::class,
+        OvertimeRequest::class => OvertimeRequestPolicy::class,
+        Payroll::class => PayrollPolicy::class,
+        PayrollPeriod::class => PayrollPeriodPolicy::class,
+        ShiftSchedule::class => ShiftSchedulePolicy::class,
     ];
 
     public function boot()
